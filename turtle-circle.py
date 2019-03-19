fa7968891c2adb791f787c340ee0cbdf5cb1e13a
john.westerman@gmail.com <john.westerman@gmail.com>
2019-03-18 19:04:43 -0500

    commit turtle-circle.py

---
+++ turtle-circle.py
@@ -0,0 +1,18 @@
+import math
+import turtle
+
+# draws a circle
+
+def drawCircleTurtle(x, y, r):
+    #move to start of circle
+    turtle.up()
+    turtle.setpos(x + r, y)
+    turtle.down()
+
+    #draw the circle
+    for i in range(0, 365, 5):
+        a = math.radians(i)
+        turtle.setpos(x + r*math.cos(a), y + r*math.sin(a))
+
+drawCircleTurtle(100,100,100)
+turtle.mainloop()
